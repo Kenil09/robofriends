5fd16d31c1363025f2621cb6d453e6e202a78fb9
Kenil09 <keniltogadiya100@gmail.com>
2022-05-03 12:05:17 +0530

    Updates

---
+++ precache-manifest.2115eeafad0fd46d790e19feef1b6675.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "741df4c0428b1165d62e",
+    "url": "/robofriends/static/css/main.73946706.chunk.css"
+  },
+  {
+    "revision": "741df4c0428b1165d62e",
+    "url": "/robofriends/static/js/main.741df4c0.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "98a5960b6b91d5f6f8ed",
+    "url": "/robofriends/static/css/2.35d01241.chunk.css"
+  },
+  {
+    "revision": "98a5960b6b91d5f6f8ed",
+    "url": "/robofriends/static/js/2.98a5960b.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "65aba4337b74b8b78eb7a230e6949e21",
+    "url": "/robofriends/index.html"
+  }
+];
